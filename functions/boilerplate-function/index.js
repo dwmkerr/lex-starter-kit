076422ade62e67ad599dc344a61032f4c0393096
Lawrence Hunt <lawrence.hunt@gmail.com>
2017-07-04 23:15:53 +1000

    Really basic interface

--- functions/boilerplate-function/index.js
+++ functions/boilerplate-function/index.js
@@ -1,5 +1,83 @@
+// --------------- Helpers to build responses which match the structure of the necessary dialog actions -----------------------
+
+function elicitSlot(sessionAttributes, intentName, slots, slotToElicit, message, responseCard) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ElicitSlot',
+            intentName,
+            slots,
+            slotToElicit,
+            message,
+            responseCard,
+        },
+    };
+}
+
+function confirmIntent(sessionAttributes, intentName, slots, message, responseCard) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'ConfirmIntent',
+            intentName,
+            slots,
+            message,
+            responseCard,
+        },
+    };
+}
+
+function close(sessionAttributes, fulfillmentState, message, responseCard) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Close',
+            fulfillmentState,
+            message,
+            responseCard,
+        },
+    };
+}
+
+function delegate(sessionAttributes, slots) {
+    return {
+        sessionAttributes,
+        dialogAction: {
+            type: 'Delegate',
+            slots,
+        },
+    };
+}
+
+// Build a responseCard with a title, subtitle, and an optional set of options which should be displayed as buttons.
+function buildResponseCard(title, subTitle, options) {
+    let buttons = null;
+    if (options != null) {
+        buttons = [];
+        for (let i = 0; i < Math.min(5, options.length); i++) {
+            buttons.push(options[i]);
+        }
+    }
+    return {
+        contentType: 'application/vnd.amazonaws.card.generic',
+        version: 1,
+        genericAttachments: [{
+            title,
+            subTitle,
+            buttons,
+        }],
+    };
+}
+
 function handler(event, context, callback) {
-  callback(null, 'Success!');
+	const projectName = event.currentIntent.slots.ProjectName;
+
+	if (!projectName) {
+		callback(null, elicitSlot(event.sessionAttributes, event.currentIntent.name, event.currentIntent.slots, 'ProjectName', 'What is the project name?'))
+	} else {
+		callback(null, close(event.sessionAttributes, 'Fulfilled', { contentType: 'PlainText',
+       content: 'Your project has 10 stars' }));
+	}
 };
 
 module.exports = {
--- makefile
+++ makefile
@@ -2,27 +2,27 @@ build:
 	cd functions/boilerplate-function && yarn install && zip -r ../../boilerplateFunction.zip .
 
 upload:
-	aws s3 cp boilerplateFunction.zip s3://lex-functions/functions/boilerplateFunction.zip
+	aws s3 cp boilerplateFunction.zip s3://lex-chatbot/functions/boilerplateFunction.zip
 
 create:
 	aws lambda create-function \
-		--region eu-west-1 \
+		--region us-east-1 \
 		--function-name boilerplateFunction \
 		--runtime nodejs6.10 \
 		--handler index.handler \
 		--role arn:aws:iam::160696617623:role/boilerplate-policy \
-		--code S3Bucket=lex-functions,S3Key=functions/boilerplateFunction.zip
+		--code S3Bucket=lex-chatbot,S3Key=functions/boilerplateFunction.zip
 
 update:
 	aws lambda update-function-code \
-    --region eu-west-1 \
+    --region us-east-1 \
     --function-name boilerplateFunction \
-    --s3-bucket lex-functions \
+    --s3-bucket lex-chatbot \
     --s3-key functions/boilerplateFunction.zip
 
 release: deploy
-	VERSION=$$(aws lambda publish-version --region eu-west-1 --function-name boilerplateFunction | jq -r .Version); \
-	aws lambda update-alias --function-name boilerplateFunction --region eu-west-1 --function-version $$VERSION --name PROD
+	VERSION=$$(aws lambda publish-version --region us-east-1 --function-name boilerplateFunction | jq -r .Version); \
+	aws lambda update-alias --function-name boilerplateFunction --region us-east-1 --function-version $$VERSION --name PROD
 
 deploy: build upload update
 
