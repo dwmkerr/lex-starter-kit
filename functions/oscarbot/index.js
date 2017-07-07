81363f939833839b116bbed0dcdc93288dfe8ba7
Lawrence Hunt <lawrence.hunt@gmail.com>
2017-07-07 15:51:12 +1000

    Add to slots when existing in session

--- functions/oscarbot/index.js
+++ functions/oscarbot/index.js
@@ -18,8 +18,18 @@ function handler(event, context, callback) {
 	const slotProjectName = getProjectNameFromSlot(event);
 
 	if (!sessionProjectName && !slotProjectName) {
+		// If neither session attribute or slot is populated then ask user for project name
 		return callback(null, dialogActions.elicitSlot(sessionAttributes, event.currentIntent.name, event.currentIntent.slots, PROJECT_SLOT, { contentType: 'PlainText', content: 'What is the project name?' }))
+	} else if (!slotProjectName && sessionProjectName) {
+		// If slot is configured in session but not yet in slot
+		// store it in slot variable and delegate back to Lex config
+		const slots = Object.assign({}, event.currentIntent.slots);
+		slots[PROJECT_SLOT] = sessionProjectName;
+
+		return callback(null, dialogActions.delegate(sessionAttributes, slots))
 	} else if (slotProjectName && !sessionProjectName) {
+		// If slot is configured but not yet stored as a session
+		// store it in session attributes and delegate back to Lex config
 		sessionAttributes.ProjectName = slotProjectName;
 		return callback(null, dialogActions.delegate(sessionAttributes, event.currentIntent.slots))
 	}
