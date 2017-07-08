dabc312e575db0d601a0bb44bfcf99352a9b18d5
Lawrence Hunt <lawrence.hunt@gmail.com>
2017-07-08 17:29:24 +1000

    Add in confirm card for opening issue

--- functions/oscarbot/intentOpenIssue.js
+++ functions/oscarbot/intentOpenIssue.js
@@ -8,12 +8,39 @@ function elicitSlot(event, slotName, message, callback) {
       event.currentIntent.name, event.currentIntent.slots, slotName, { contentType: 'PlainText', content: message } ));
 }
 
+function checkConfirmationStatus(event, callback) {
+  const confirmationStatus = event.currentIntent.confirmationStatus;
+  if (confirmationStatus === 'Denied') {
+    return callback(null, dialogActions.close(event.sessionAttributes, 'Fulfilled', {
+      contentType: 'PlainText',
+      content: 'Ok, I will not create the issue.'
+    }));
+  } else if (confirmationStatus === 'None') {
+    return callback(null, dialogActions.confirmIntent(event.sessionAttributes,
+      event.currentIntent.name,
+      event.currentIntent.slots,
+      {
+        contentType: 'PlainText',
+        content: `Are you sure you want to open an issue titled ${event.currentIntent.slots.IssueTitle} with description ${event.currentIntent.slots.IssueContent}?`
+      },
+      dialogActions.buildResponseCard('Confirm', null, [{
+        text: 'Yes',
+        value: 'Yes'
+      }, {
+        text: 'No',
+        value: 'No'
+      }])))
+  }
+}
+
 function handler(event, context, callback) {
   const issueTitle = event.currentIntent.slots.IssueTitle;
   const issueContent = event.currentIntent.slots.IssueContent;
 
   if (!issueTitle) return elicitSlot(event, 'IssueTitle', 'Can you give a me a title for the issue?', callback);
   if (!issueContent) return elicitSlot(event, 'IssueContent', 'Can you give a me a description for the issue', callback);
+
+  if (checkConfirmationStatus(event, callback)) return;
 
   github.login(config.GITHUB_USERNAME, config.GITHUB_PASSWORD)
     .then((token) => {
