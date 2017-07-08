62f94df495fa39448b98286ab3cccae8c23b8231
Dave Kerr <dwmkerr@gmail.com>
2017-07-08 23:14:35 +0700

    feature: added the describe last commit intent

---
+++ functions/oscarbot/intentDescribeLastCommit.spec.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const { handler } = require('./index');
+const createTestInput = require('./tests/createTestInput');
+
+describe('intentDescribeLastCommit', () => {
+  it('should be able to describe the last commit for a project', (done) => {
+    const input = createTestInput({
+      intent: 'DescribeLastCommit',
+      sessionAttributes: { Repository: 'dwmkerr/sharpgl' }
+    });
+
+    handler(input, null, (err, response) => {
+      assert.equal(response.dialogAction.type, 'Close');
+      assert(response.dialogAction.message.content.match(/last commit on dwmkerr\/sharpgl was/));
+      done();
+    });
+  });
+});
