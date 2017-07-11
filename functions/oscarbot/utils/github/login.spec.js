7293f7f08408bfb9c38744360cde4497ec86aad3
Lawrence Hunt <lawrence.hunt@gmail.com>
2017-07-11 19:02:32 +1000

    Fix linting

--- functions/oscarbot/utils/github/login.spec.js
+++ functions/oscarbot/utils/github/login.spec.js
@@ -4,38 +4,38 @@ const config = require('../../config');
 
 describe('Github login', () => {
   it('Should respond with a token', (done) => {
-  	login(config.GITHUB_USERNAME, config.GITHUB_PASSWORD, {})
-  		.then((token) => {
-  			assert(token.match(/.+/));
-  			done();
-  		});
+    login(config.GITHUB_USERNAME, config.GITHUB_PASSWORD, {})
+      .then((token) => {
+        assert(token.match(/.+/));
+        done();
+      });
   });
 
   it('Should set sessionAttribute token', (done) => {
-  	const event = {};
+    const event = {};
 
-  	login(config.GITHUB_USERNAME, config.GITHUB_PASSWORD, event)
-  		.then((token) => {
-  			assert.equal(event.sessionAttributes.GithubToken, token);
-  			done();
-  		});
+    login(config.GITHUB_USERNAME, config.GITHUB_PASSWORD, event)
+      .then((token) => {
+        assert.equal(event.sessionAttributes.GithubToken, token);
+        done();
+      });
   });
 
   it('Should use sessionAttribute token', (done) => {
-  	const event = {
-  		sessionAttributes: {
-  			GithubToken: '123abc'
-  		}
-  	};
+    const event = {
+      sessionAttributes: {
+        GithubToken: '123abc'
+      }
+    };
 
-  	login(config.GITHUB_USERNAME, config.GITHUB_PASSWORD, event)
-  		.then((token) => {
-  			assert.equal(token, '123abc');
-  			done();
-  		});
+    login(config.GITHUB_USERNAME, config.GITHUB_PASSWORD, event)
+      .then((token) => {
+        assert.equal(token, '123abc');
+        done();
+      });
   });
 
   it('Should throw error with no username or password', () => {
-  	assert.throws(login, Error);
+    assert.throws(login, Error);
   });
 });
