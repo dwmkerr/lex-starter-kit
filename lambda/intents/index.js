311bbd9923bb847f3163ffc92a6f1b153e2bac0a
Dave Kerr <dwmkerr@gmail.com>
2019-08-26 11:43:08 +0800

    refactor: remove stale code

--- lambda/intents/index.js
+++ lambda/intents/index.js
@@ -1,8 +1,6 @@
 //  Important! Each intent exported from this module *must* have a key which
 //  matches an intent in an intent file.
 module.exports = {
-  CreateIssue: require('./createIssue'),
-  SendMessage: require('./sendMessage'),
   WhatTime: require('./whatTime'),
   WhatDate: require('./whatDate'),
   WhatCapitalCity: require('./whatCapitalCity'),
